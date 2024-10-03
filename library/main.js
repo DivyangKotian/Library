de969f824a0807a9163e8c7a519d63eaa9075e57
Divyang Kotian <kotian.divyang@gmail.com>
2024-10-03 18:11:00 -0400

    book card creation added, read status check added with functionality

--- library/main.js
+++ library/main.js
@@ -21,6 +21,30 @@ function createBookElements(elementName, content, className) {
     return elementVar;
 }
 
+function createRead(bookContainer,book){
+    const read= document.createElement('div');
+    read.appendChild(createBookElements('h1', 'Read Status', 'read-status'));
+    let readBtn= document.createElement('input');
+    readBtn.type = "checkbox";
+    
+    readBtn.addEventListener('click', (e)=>{
+        if(e.target.checked){
+            bookContainer.setAttribute('class', 'card-container book read-true');
+            book.read=true;
+        }
+        else{
+            bookContainer.setAttribute('class', 'card-container book read-false')
+            book.read=false;
+        }
+    });
+    if(book.read){
+        readBtn.checked=true;
+        bookContainer.setAttribute('class', 'card-container book read-true')
+    }
+    read.appendChild(readBtn);
+    return read;
+}
+
 // Function to create the book container for each book
 function createBookContainer(book, index) {
     const bookContainer = document.createElement('div');
@@ -38,17 +62,19 @@ function createBookContainer(book, index) {
     bookContainer.appendChild(
         createBookElements('h1', `Pages: ${book.pages}`, 'book-pages')
     );
-    bookContainer.appendChild(
-        createBookElements('h1', `Read-Status: ${book.read ? 'Read' : 'Not Read'}`, 'book-read-status')
-    );
+    bookContainer.appendChild(createRead(bookContainer,book));
     
-    // Insert the book container into the DOM
-    books.insertAdjacentElement("afterbegin", bookContainer);
+    
+    // Insert the book container into the DOM, I'm going for a chronological method, so oldest book stays first child of the container div, so new elements added before end
+    //using an alternative to appendChild() for practice, but with insetAdjacentElement i can make it so that the new book
+    //is the first child, hence we can make a recently added section etc
+
+    books.insertAdjacentElement("beforeend", bookContainer);
 }
 
 // Function to render the book library
 function booksRender() {
-    bookLibrary.forEach((book, index) => {
+    bookLibrary.map((book, index) => {
         createBookContainer(book, index);
     });
 }
